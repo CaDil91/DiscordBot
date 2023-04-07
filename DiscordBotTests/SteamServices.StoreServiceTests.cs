b6109c94e683048d1a5f1fa0980e6b52051b91fb
CaDil91 <caleb.dilworth@gmail.com>
2023-04-06 22:42:16 -0500

    Added Steam Store Service: - Added named http client to DI container. Had to be named for Moq, since the base method of CreateClient() without params is virtual I believe, and can't be instantiated. - Passes Unit Test to ping https://store.steampowered.com.

---
+++ DiscordBotTests/SteamServices.StoreServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using Moq;
+using Moq.Language.Flow;
+using Moq.Protected;
+using SteamServices;
+
+namespace DiscordBot;
+
+public class SteamServicesStoreServiceTests
+{
+    private readonly StoreService _subjectUnderTest;
+    private readonly Mock<IHttpClientFactory> _mockHttpClientFactory = new();
+
+    public SteamServicesStoreServiceTests()
+    {
+        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+        var result = new HttpResponseMessage();
+        handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(result)
+            .Verifiable();
+
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("https://store.steampowered.com"),
+        };
+
+        _mockHttpClientFactory.Setup(_ => _.CreateClient("hardcodedsteam")).Returns(httpClient);
+        
+        _subjectUnderTest = new StoreService(_mockHttpClientFactory.Object);
+    }
+    
+    [Fact]
+    public async Task SearchStoreAsync_CanMakeHttpRequestsToStore()
+    {
+        //Arrange.
+        //Act.
+        HttpResponseMessage response = await _subjectUnderTest.RequestSteamStore();
+        
+        //Assert.
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task SearchStoreAsync_ReturnsListOfAppIds()
+    {
+        //Arrange.
+        
+        //Act.
+        
+        //Assert.
+    }
+
+    [Fact]
+    public async Task SearchStoreAsync_ReturnsErrorResponseIfNoAppsFound()
+    {
+        //Arrange.
+
+        //Act.
+
+        //Assert.
+    }
+}
---
+++ SteamServices/StoreService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SteamServices;
+
+public class StoreService
+{
+    private readonly IHttpClientFactory  _httpClientFactory;
+    private readonly HttpClient _httpClient;
+    public enum SortBy 
+    {
+        MostPopular,
+    }
+
+    public StoreService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _httpClient = _httpClientFactory.CreateClient("hardcodedsteam");
+    }
+
+    /// <summary>
+    /// Search the Steam store by a given search term.
+    /// </summary>
+    /// <param name="searchTerm">The search parameter to query to the Steam Store by.</param>
+    /// <returns>List of App Id's for the given search</returns>
+    public async Task<List<JObject>> GetAppFromStoreAsync(string? searchTerm, int iAppReturnCountMax = 10, List<SortBy> listSortBy = null)
+    {
+        List<JObject> retListAppIds = new() { new JObject() };
+
+        HttpResponseMessage responseMessage = await _httpClient.GetAsync(new Uri($"https://store.steampowered.com/app/{searchTerm}"));
+        
+        //throw new Exception("Not yet implemented");
+        return retListAppIds;
+    }
+
+    public async Task<HttpResponseMessage> RequestSteamStore() => await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "https://store.steampowered.com/app/"));
+
+}
