258763ad068a8ae5d41ee4a8c6c5688e3b49ab64
CaDil91 <caleb.dilworth@gmail.com>
2023-05-07 19:21:52 -0500

    Ok, with StoreServiceTests.cs for now I guess. Not much to test really in GetAppsAsync since it passes the logic on to it's repo object.

--- DiscordBotTests/SteamServices/StoreServiceTests.cs
+++ DiscordBotTests/SteamServices/StoreServiceTests.cs
@@ -1,6 +1,4 @@
-﻿using AzureServices;
-using Microsoft.Extensions.Options;
-using Moq;
+﻿using Moq;
 using SteamServices;
 
 namespace DiscordBot.SteamServices;
@@ -12,35 +10,34 @@ public class SteamServicesStoreServiceTests
     public SteamServicesStoreServiceTests()
     {
         // Create mocks
-        Mock<IHttpClientFactory> httpClientFactoryMock = new();
-
-        //Create optional setup for the IOptions<SteamOptions> mock.
-        Mock<IOptions<SteamOptions>> steamOptionsMock = new();
-        steamOptionsMock.Setup(x => x.Value).Returns(new SteamOptions());
-        
-        // Setup the HttpClientFactory mock to return a new HttpClient.
-        httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(new HttpClient());
+        Mock<IAppRepository> appRepositoryMock = new();
+        appRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(new List<SteamApp>()
+        {
+            new() { AppId = 1, Name = "App 1" },
+            new() { AppId = 2, Name = "App 2" },
+            new() { AppId = 3, Name = "App 3" }
+        });
 
         // Create subject under test.
-        _subjectUnderTest = new StoreService(httpClientFactoryMock.Object);
+        _subjectUnderTest = new StoreService(appRepositoryMock.Object);
     }
-
     /// <summary>
-    /// GetAppsFromStoreAsync() test.
+    /// GetAppsAsync() test.
     /// </summary>
     [InlineData("counter strike", 5)]
     [InlineData("d001441f-738b-43fc-a5d5-2b1225490c1fd001441f-738b-43fc-a5d5-2b1225490c1f", 10)]
     [InlineData("halo", 3)]
+    [InlineData("", 1)]
+    [InlineData("halo", -10)]
     [Theory]
-    public async Task GetAppsFromStoreAsync_ReturnsBetweenZeroAndRequestedAppReturnCount(string sValidSearchTerm, 
-        int iAppReturnCountMax)
+    public async Task GetAppsAsync_ReturnsListOfApps(string sValidSearchTerm, int iAppReturnCountMax)
     {
         // Arrange.
         
         // Act.
-        List<SteamApp> steamApps = await _subjectUnderTest.GetAppsFromStoreAsync(sValidSearchTerm, iAppReturnCountMax);
+        List<SteamApp> steamApps = await _subjectUnderTest.GetAppsAsync(sValidSearchTerm, iAppReturnCountMax);
 
         // Assert.
-        Assert.InRange(steamApps.Count, 0, iAppReturnCountMax);
+        Assert.NotNull(steamApps);
     }
 }
